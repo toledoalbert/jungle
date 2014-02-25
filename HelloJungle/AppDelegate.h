046b07de54eb33bc3f03743da48931cbd3e1436c
Albert Toledo <toledoalbert@gmail.com>
2014-02-25 06:23:02 -0500

    Initial Commit

---
+++ HelloJungle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloJungle
+//
+//  Created by Albert Toledo on 2/25/14.
+//  Copyright (c) 2014 Albert Toledo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
