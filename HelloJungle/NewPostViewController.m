078660c5c819852be1b4c3e43b6c9807517c295c
Albert Toledo <toledoalbert@gmail.com>
2014-02-27 10:47:25 -0500

    Created new view controller for post creation as well as the connections for their views.

---
+++ HelloJungle/NewPostViewController.m
@@ -0,0 +1,48 @@
+//
+//  NewPostViewController.m
+//  HelloJungle
+//
+//  Created by Albert Toledo on 2/27/14.
+//  Copyright (c) 2014 Albert Toledo. All rights reserved.
+//
+
+#import "NewPostViewController.h"
+
+@interface NewPostViewController ()
+
+@end
+
+@implementation NewPostViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)takePicture:(id)sender {
+}
+
+- (IBAction)selectPicture:(UIButton *)sender {
+}
+
+- (IBAction)selectPicture:(id)sender {
+}
+- (IBAction)takePicture:(UIButton *)sender {
+}
+@end
