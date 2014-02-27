bba586ce2115d56a88dacd9d8cc53af180ca0f53
Albert Toledo <toledoalbert@gmail.com>
2014-02-26 22:22:09 -0500

    Horizontal swipe view works with data loaded from the models.

--- HelloJungle/HomeViewController.h
+++ HelloJungle/HomeViewController.h
@@ -23,6 +23,8 @@
 //Attributes for data
 @property (strong, nonatomic) User *currentUser; //Most of the data will come from User's attributes.
 @property (strong, nonatomic) Feed *usersFeed;
+@property (strong, nonatomic) IBOutlet UILabel *labelQuestion;
+@property (strong, nonatomic) IBOutlet UIImageView *postImage;
 
 
 @end
--- HelloJungle/HomeViewController.m
+++ HelloJungle/HomeViewController.m
@@ -30,22 +30,22 @@
     ////////SAMPLE DATA/////////
     Post *post1 = [[Post alloc] init];
     post1.user =  [[User alloc] init];
-    post1.image = [[UIImage alloc] initWithContentsOfFile:@"image1.jpg"];
+    post1.image = [[UIImage alloc] initWithContentsOfFile:@"/Users/toledoalbert/Documents/iOS/HelloJungle/HelloJungle/image1.jpg"];
     post1.content = @"Where is Post1?";
     
     Post *post2 = [[Post alloc] init];
     post2.user =  [[User alloc] init];
-    post2.image = [[UIImage alloc] initWithContentsOfFile:@"image2.jpg"];
+    post2.image = [[UIImage alloc] initWithContentsOfFile:@"/Users/toledoalbert/Documents/iOS/HelloJungle/HelloJungle/image2.jpg"];
     post2.content = @"Where is Post2?";
     
     Post *post3 = [[Post alloc] init];
     post3.user =  [[User alloc] init];
-    post3.image = [[UIImage alloc] initWithContentsOfFile:@"image3.jpeg"];
+    post3.image = [[UIImage alloc] initWithContentsOfFile:@"/Users/toledoalbert/Documents/iOS/HelloJungle/HelloJungle/image3.jpeg"];
     post3.content = @"Where is Post3?";
     
     Post *post4 = [[Post alloc] init];
     post4.user =  [[User alloc] init];
-    post4.image = [[UIImage alloc] initWithContentsOfFile:@"image4.jpg"];
+    post4.image = [[UIImage alloc] initWithContentsOfFile:@"/Users/toledoalbert/Documents/iOS/HelloJungle/HelloJungle/image4.jpg"];
     post4.content = @"Where is Post4?";
     
 
@@ -70,8 +70,10 @@
     //configure swipe view
     _swipeView.alignment = SwipeViewAlignmentCenter;
     _swipeView.pagingEnabled = YES;
-    _swipeView.itemsPerPage = 2;
+    _swipeView.itemsPerPage = 1;
     _swipeView.truncateFinalPage = YES;
+    
+    
     //Removing the Background "back" button
     self.navigationItem.leftBarButtonItem=nil;
     self.navigationItem.hidesBackButton=YES;
@@ -87,15 +89,16 @@
     //that the user has in his/her feed.
     //This number will be returned from the User objects, feed property's
     //posts array
-    return 10;//_usersFeed.posts.count; //Number of the posts in the feed retrieved from the attribute.
+    return _usersFeed.posts.count; //Number of the posts in the feed retrieved from the attribute.
 }
 
 - (UIView *)swipeView:(SwipeView *)swipeView viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view
 {
     
     //Get the data from model
-    //NSString *content = ((Post *)_usersFeed.posts[index]).content;
-    //UIImageView *image = [[UIImageView alloc] initWithImage:((Post *)_usersFeed.posts[index]).image];
+    Post *currentPost = ((Post *)_usersFeed.posts[index]);
+    NSString *contentFromPost = currentPost.content;
+    UIImage *imageFromPost = currentPost.image;
     
     
     if (!view)
@@ -103,6 +106,9 @@
         //Assign the data from model to the ui elements.
     	//view = [[UIView alloc] initWithFrame:self.swipeView.bounds];
         view = [[NSBundle mainBundle] loadNibNamed:@"PostView" owner:self options:nil][0];
+        
+        _labelQuestion.text = contentFromPost;
+        _postImage.image = currentPost.image;
 
     }
     
