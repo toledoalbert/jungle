93e42c59ac9dafe7e61b3cc82e08d6c34ec9561e
Albert Toledo <toledoalbert@gmail.com>
2014-03-13 00:46:22 -0400

    Move the comments to the external xib file.

---
+++ HelloJungle/CommentsView.h
@@ -0,0 +1,15 @@
+//
+//  CommentsView.h
+//  HelloJungle
+//
+//  Created by Albert Toledo on 3/13/14.
+//  Copyright (c) 2014 Albert Toledo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentsView : UIView
+
+- (id)initWithCoder:(NSCoder *)aDecoder;
+
+@end
---
+++ HelloJungle/CommentsView.m
@@ -0,0 +1,38 @@
+//
+//  CommentsView.m
+//  HelloJungle
+//
+//  Created by Albert Toledo on 3/13/14.
+//  Copyright (c) 2014 Albert Toledo. All rights reserved.
+//
+
+#import "CommentsView.h"
+
+@implementation CommentsView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    if ((self = [super initWithCoder:aDecoder])) {
+        [self addSubview:[[[NSBundle mainBundle] loadNibNamed:@"CommentsView" owner:self options:nil] objectAtIndex:0]];
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
